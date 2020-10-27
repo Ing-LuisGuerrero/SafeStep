4110bee0728f6bde64c26d3e51408b59952357b4
Ing-LuisGuerrero <manuel.guerreroalns@uanl.edu.mx>
2020-10-27 13:13:23 -0600

    First steps.

--- app/src/main/java/com/equipo5/safestep/MainActivity.kt
+++ app/src/main/java/com/equipo5/safestep/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.equipo5.safestep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.animation_splash)
+        tvAppName.startAnimation(animation)
+
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+            }
+
+        })
+
     }
 }
