9abffbfd2c4abe2a0b03c2ccc7728803d69a0f11
Ing-LuisGuerrero <manuel.guerreroalns@uanl.edu.mx>
2020-10-27 20:13:07 -0600

    Splashscreen, login and signup done and connected.

--- app/src/main/java/com/equipo5/safestep/LoginActivity.kt
+++ app/src/main/java/com/equipo5/safestep/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.equipo5.safestep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnGoToSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/equipo5/safestep/SignUpActivity.kt
@@ -0,0 +1,18 @@
+package com.equipo5.safestep
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
+import kotlinx.android.synthetic.main.activity_signup.*
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        val text =  "Acepto los <b><font color=#F85F6A>Terminos, Condiciones</font></b> y el aviso de privacidad."
+        tvTermsConditions.text = HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        btnGotoLogin.setOnClickListener { finish() }
+    }
+}
