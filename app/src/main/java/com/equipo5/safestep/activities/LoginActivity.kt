16779bf21b263a8acc36416e0a7e723ba66eaad2
Ing-LuisGuerrero <manuel.guerreroalns@uanl.edu.mx>
2020-10-30 01:04:49 -0600

    SignUp code rewritten and working.

--- app/src/main/java/com/equipo5/safestep/activities/LoginActivity.kt
+++ app/src/main/java/com/equipo5/safestep/activities/LoginActivity.kt
@@ -3,12 +3,16 @@ package com.equipo5.safestep.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.equipo5.safestep.R
 import com.equipo5.safestep.ValidateEmail
 import com.equipo5.safestep.providers.AuthProvider
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlin.math.log
 
@@ -74,7 +78,14 @@ class LoginActivity : AppCompatActivity(), ValidateEmail {
                 }
 
             } else {
-                Toast.makeText(this, getString(R.string.invalid_credentials), Toast.LENGTH_LONG).show()
+                try {
+                    throw it.exception!!
+                } catch (e: FirebaseAuthInvalidCredentialsException) {
+                    Toast.makeText(this, getString(R.string.invalid_credentials), Toast.LENGTH_LONG).show()
+                } catch (e: Exception){
+                    Toast.makeText(this, getString(R.string.error_operation), Toast.LENGTH_LONG).show()
+                    Log.e("Error", "Error generico")
+                }
             }
         }
     }
